54a3de525da3aaa5544cb862417ad7abad386145
JasonCodez <jason13garrett@gmail.com>
2021-09-23 17:03:11 -0500

    updated app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY', 'hellosecret2')
 
 
 connect_db(app)
-db.drop_all()
+# db.drop_all()
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
